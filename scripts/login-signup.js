5e18aa15cdac8ee9d641e330d6479a3d43f48080
Ali Shouman <alishouman010@gmail.com>
2022-09-11 14:10:12 +0300

    enter error code

--- scripts/login-signup.js
+++ scripts/login-signup.js
@@ -5,36 +5,66 @@ const userName_login = document.querySelector("#userName-login");
 const password_login = document.querySelector("#password-login");
 const login = document.querySelector("#login");
 const signup = document.querySelector("#signup");
+const error = document.querySelector("#error");
 
+// add user signup
 signup.addEventListener("click", (e) => {
   let userName = userName_sigup.value;
   let password = password_sigup.value;
+
+  // entered value not nulls
   if (userName != "" && password != "") {
-    let user = {
+    let data = JSON.parse(localStorage.getItem("users"));
+    let addUser = {
       userName: userName,
       password: password,
     };
-
-    let user_json = JSON.stringify(user);
-    localStorage.setItem("user", user_json);
+    // add user or append if it exist
+    if (data == null) data = [];
+    data.push(addUser);
+    localStorage.setItem("users", JSON.stringify(data));
     console.log("add done");
   } else {
-    console.log("add not done");
+    errorEnter();
   }
 
   e.preventDefault();
 });
 
+// login
 login.addEventListener("click", (e) => {
   let userName = userName_login.value;
   let password = password_login.value;
+  let login = false;
+
+  // entered value not null
   if (userName != "" && password != "") {
-    let data = localStorage.getItem("user");
-    console.log(data);
-    data = JSON.parse(data);
-    console.log(data);
+    let data = JSON.parse(localStorage.getItem("users"));
+    let user;
+    // search for user
+    for (let i = 0; i < data.length; i++) {
+      user = data[i];
+      if (user["userName"] == userName && user["password"] == password) {
+        console.log("loign done");
+        window.location.replace("prediction.html");
+        login = true;
+        break;
+      }
+    }
+    if (!login) {
+      errorEnter();
+    }
   } else {
-    console.log();
+    errorEnter();
   }
   e.preventDefault();
 });
+
+function removeError() {
+  error.classList.add("d-none");
+}
+
+function errorEnter() {
+  error.classList.remove("d-none");
+  setTimeout(removeError, 3000);
+}
